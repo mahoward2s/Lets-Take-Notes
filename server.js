498d417e168574160522d9ee520daea6513c1cb4
Sportsguy <mahoward2s@gmail.com>
2021-02-03 14:31:08 -0600

    changed names of files, and made some changes to to the code

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.use(express.json());
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
+require('./routes/APIRoutes')(app);
+require('./routes/HTMLRoutes')(app);
 
 // LISTENER
 // The below code effectively "starts" our server
