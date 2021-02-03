30bca2e99f765cbc53428d11ec399ba50f95822c
Sportsguy <mahoward2s@gmail.com>
2021-02-03 14:54:33 -0600

    made a small adjustment to the code....still needs work

--- routes/APIRoutes.js
+++ routes/APIRoutes.js
@@ -1,9 +1,7 @@
 // LOAD DATA
 // We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
 
-const tableData = require('../data/tableData');
-const waitListData = require('../data/waitinglistData');
+const storedNotes = require('../db/store');
 
 // ROUTING
 
