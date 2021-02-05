b05d4f9fd07ee5d69c8921d937085ee08b62b5b6
Sportsguy <mahoward2s@gmail.com>
2021-02-04 19:51:35 -0600

    forgot a _

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 8080;
   // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(_dirname));
+app.use(express.static(__dirname));
 
 // ROUTER
 // The below points our server to a series of "route" files.
