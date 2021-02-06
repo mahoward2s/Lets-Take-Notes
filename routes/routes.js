16e400016c2ce8e1a9f1bbb24cac889cba1961dd
Sportsguy <mahoward2s@gmail.com>
2021-02-06 11:36:31 -0600

    made changes

--- routes/routes.js
+++ routes/routes.js
@@ -5,30 +5,36 @@ const path = require('path');
 //------------------------------------ API Routes-------------------------------------
 
 module.exports = (app) => {
-    //sets note variable
-    fs.readFile('db/db.json', 'utf8', (err, data) => {
-        if (err) throw err;
-        var notes = JSON.parse(data);
-
         // API GET Requests
-        app.get('/api/notes', (req, res) => res.json(notes));
+        app.get('/api/notes', (req, res) => {
+            let notes = readDb();
+            return res.json(notes);
+        });
 
         // API POST Requests
         app.post('/api/notes', (req, res) => {
             let newNote = req.body;
+            let notes = readDb();
+            let noteId = (notes.length).toString();
+            newNote.id = noteId
             notes.push(newNote);
-            updateDb();
-            return console.log('Successfully added new note: ' + newNote.title);
+            updateDb(notes);
+            return res.json(notes), console.log('Successfully added new note: ' + newNote.title);
         });
 
         //Retrieves a note with a specific id
-        app.get('/api/notes/:id', (req, res) => res.json(notes[req.params.id]));
+        app.get('/api/notes/:id', (req, res) => {
+            let notes = readDb();
+            return res.json(notes[req.params.id])
+        });
 
         // API delete request
         app.delete('/api/notes/:id', (req, res) => {
+            let notes = readDb();
+            app.get('/api/notes/:id', (req, res) => res.json(notes[req.params.id]));
             notes.splice(req.params.id, 1);
-            updateDb();
-            return console.log('Successfully deleted note with id: ' + req.params.id);
+            updateDb(notes);
+            return res.json(notes), console.log('Successfully deleted note with id: ' + req.params.id);
         });
 
         // ----------------------------------------HTML ROUTES--------------------
@@ -45,11 +51,16 @@ module.exports = (app) => {
         //------------------------------------------------------------------------
 
         //updates the json file whenever a note is added or deleted
-        function updateDb() {
-            fs.writeFile('db/db.json', JSON.stringify(notes, '\t'), err => {
+        function updateDb(notes) {
+            fs.writeFileSync('db/db.json', JSON.stringify(notes, '\t'), err => {
                 if (err) throw err;
                 return true;
             });
         }
-    });
+        function readDb (){
+            const data = fs.readFileSync('db/db.json', 'utf8')
+            var notes = JSON.parse(data);
+            return notes;
+        }
+    
 };
